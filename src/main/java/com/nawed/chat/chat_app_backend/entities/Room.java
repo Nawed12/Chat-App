c9b562ff6410fb20b474ff7cb5bbf304ac9368f2
Nawed <nawedhero786@gmail.com>
2026-03-15 00:48:27 +0530

    Entity

---
+++ src/main/java/com/nawed/chat/chat_app_backend/entities/Room.java
@@ -0,0 +1,22 @@
+package com.nawed.chat.chat_app_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collation = "rooms")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+    @Id
+    private String id;
+    private String roomId;
+    private List<Message> messages=new ArrayList<>();
+}
